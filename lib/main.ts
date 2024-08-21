f51b90aa07856eef044c0bbecd7db062f0f2b8ab
shu0725 <wspkiki1992@cycu.org.tw>
2024-08-21 17:18:51 +0800

    v1.20

--- lib/main.ts
+++ lib/main.ts
@@ -2,10 +2,13 @@
 import libEn from '@i18n/en/lib.json';
 import libZhTw from '@i18n/zh-TW/lib.json';
 import SearchDateTimeRangePicker from './components/search/DateTimeRangePicker';
+import SearchDateRangePicker from './components/search/DateRangePicker';
 
 export { Input } from './components/Input';
 export { Label } from './components/Label';
 export { SearchDateTimeRangePicker };
+export { SearchDateRangePicker };
+
 export { Android12Switch } from './components/switch/CustomizedSwitches';
 export { libEn };
 export { libZhTw };
