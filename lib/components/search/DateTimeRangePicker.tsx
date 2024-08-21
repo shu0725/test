e5e9c5157a7483cadcd5465c367d283dd8cb1516
shu0725 <wspkiki1992@cycu.org.tw>
2024-08-21 14:25:43 +0800

    v1.2

--- lib/components/search/DateTimeRangePicker.tsx
+++ lib/components/search/DateTimeRangePicker.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useMemo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
-import i18n from '@i18n/index';
 import { getDateRange } from '@utils/date';
 // material-ue
 import { FormControl, Button } from '@mui/material';
--- lib/main.ts
+++ lib/main.ts
@@ -5,7 +5,7 @@ import SearchDateTimeRangePicker from './components/search/DateTimeRangePicker';
 
 export { Input } from './components/Input';
 export { Label } from './components/Label';
-export { SearchDateTimeRangePicker };
+export type { SearchDateTimeRangePicker };
 export { Android12Switch } from './components/switch/CustomizedSwitches';
 export { libEn };
 export { libZhTw };
